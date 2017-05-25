e3d1c4ccb00f8d5526b4cc88391d57e899a68ba3
vicvans20 <vicvans20@gmail.com>
2017-05-25 15:44:00 -0600

    Add coherence router setup.

--- lib/yt_logger/web/router.ex
+++ lib/yt_logger/web/router.ex
@@ -1,5 +1,6 @@
 defmodule YtLogger.Web.Router do
   use YtLogger.Web, :router
+  use Coherence.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -7,11 +8,33 @@ defmodule YtLogger.Web.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session
   end
 
   pipeline :api do
     plug :accepts, ["json"]
   end
+
+  # Coherence!
+  pipeline :protected do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session, protected: true
+  end
+
+  scope "/" do
+    pipe_through :browser
+    coherence_routes()
+  end
+
+  scope "/" do
+    pipe_through :protected
+    coherence_routes :protected
+  end
+  # ---------------
 
   scope "/", YtLogger.Web do
     pipe_through :browser # Use the default browser stack
@@ -19,12 +42,13 @@ defmodule YtLogger.Web.Router do
     get "/", PageController, :index
     get "/logger", PageController, :history
     get "/oauth_app", PageController, :yt_oauth
-
-    resources "/users", UserController
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", YtLogger.Web do
-  #   pipe_through :api
-  # end
+  scope "/", YtLogger.Web do
+    pipe_through :protected
+
+    # add protected resources below
+    resources "/privates", MyProject.PrivateController
+  end
+
 end
